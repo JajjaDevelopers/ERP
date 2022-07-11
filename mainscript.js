5f40986485339826715fba1e440f59163ebeb64b
dev global <kiboolif@gmail.com>
2022-07-11 09:23:29 +0300

    improved on the javascript code,-mEnhanced the index file

--- mainscript.js
+++ mainscript.js
@@ -1,28 +1,53 @@
-arabica_grades = ["Arabica A", "Arabica AA", "Arabica AAA", "Arabica B", "Arabica AB", "Arabica CPB", "Triage", "Black Beans"];
-robusta_grades = ["Screen 1800", "Screen 1700", "Screen 1500", "Screen 1200", "Black Beans", "BHP", "1899"];
 
-
-
-
-function selectGrade(typeEntryID) {
-
-    document.getElementById("grnCoffeeGrade").innerHTML.list = []
-    let coffee_type = document.getElementById(typeEntryID).innerHTML;
-    let coffee_grades = ""
-    if (coffee_type = "Robusta") {
-        for (let grade of robusta_grades) {
-            coffee_grades = document.createElement("option");
-            coffee_grades.innerHTML = grade;
-            document.getElementById("coffeeGrades").appendChild(coffee_grades);
-        }
-    } else {
-        for (let grade of arabica_grades) {
-            coffee_grades = document.createElement("option");
-            coffee_grades.innerHTML = grade;
-            document.getElementById("coffeeGrades").appendChild(coffee_grades);
-        }
-    }
-    
+const coffeeGrade={
+ arabica_grades:["Arabica A", "Arabica AA", "Arabica AAA", "Arabica B", "Arabica AB", "Arabica CPB", "Triage", "Black Beans"],
+ robusta_grades:["Screen 1800", "Screen 1700", "Screen 1500", "Screen 1200", "Black Beans", "BHP", "1899"]
 }
+
+const option=document.getElementById("select");
+const checkedOption=document.querySelectorAll(".coffee");
+let choice;
+checkedOption.forEach(checked=>{
+  checked.addEventListener("click",(event)=>{
+    choice=event.target.id;
+    let selection="<option>"+""+"</option>"; 
+
+        if (choice==="ar"){
+            for(let i=0;i<coffeeGrade.arabica_grades.length;i++){
+                selection+="<option>"+coffeeGrade.arabica_grades[i]+"</option>"
+            }
+            option.innerHTML=selection;
+        } else{
+            for(let i=0;i<coffeeGrade.robusta_grades.length;i++){
+                selection+="<option>"+coffeeGrade.robusta_grades[i]+"</option>"
+            }
+            option.innerHTML=selection;
+        }
+  })
+})
+
+
+
+
+// function selectGrade(typeEntryID) {
+
+//     document.getElementById("grnCoffeeGrade").innerHTML.list = []
+//     let coffee_type = document.getElementById(typeEntryID).innerHTML;
+//     let coffee_grades = ""
+//     if (coffee_type = "Robusta") {
+//         for (let grade of robusta_grades) {
+//             coffee_grades = document.createElement("option");
+//             coffee_grades.innerHTML = grade;
+//             document.getElementById("coffeeGrades").appendChild(coffee_grades);
+//         }
+//     } else {
+//         for (let grade of arabica_grades) {
+//             coffee_grades = document.createElement("option");
+//             coffee_grades.innerHTML = grade;
+//             document.getElementById("coffeeGrades").appendChild(coffee_grades);
+//         }
+//     }
+    
+// }
 
 

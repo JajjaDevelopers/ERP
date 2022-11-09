d544ed1eedc22835d5bde989f9ae34566b6433c3
isaac glob <isaacwasukira@gmail.com>
2022-11-09 21:17:14 +0300

    upadates

---
+++ home/forms/drying.php
@@ -0,0 +1,50 @@
+<?php include("header.php") ?>
+
+<form class="regularForm">
+   <h3 class="formHeading">DRYING FORM</h3>
+   <h2>CUSTOMER DETAILS: <input></h2>
+   CUSTOMER ID: <INput>
+   <div> Date:<input type="date">
+    <br>MC IN:<input class="shortInput">
+    <br>MC OUT:<input class="shortInput">
+    <br>Reference:<input class="shortInput">
+  </div container>
+    <br>
+   CUSTOMER NAME: <INput>
+   <br>CUSTOMER CONTACT: <INput>
+   <h2>DESCRIPTIONS</h2>
+  <table>
+    <tr><th>DETAILS</th>
+        <th>GRADE</th>
+        <th>QUANTITY</th>
+    </tr>
+    <tr>
+     <td>INPUT</td>
+<td><input></td>
+<td><input></td>
+    </tr>
+    <tr>
+        <td>OUTPUT</td>
+        <td><input></td>
+        <td><input></td>
+    </tr>
+    <tr>
+      <td>LOSS</td>
+      <td><input></td>
+      <td><input></td>
+    </tr>
+  </table>
+  <p>
+    <div class="div1">Prepared by;</div>
+    <div class="div2">Received by;</div>
+    <div class="div3">Verified by;</div>
+    <br>
+    <div class="div1"><input></div>
+    <div class="input2"><input></div>
+    <div class="div6"><input></div>
+</p>
+
+
+</form>
+
+<?php include("footer.php") ?>
--- home/forms/header.php
+++ home/forms/header.php
@@ -202,7 +202,7 @@ session_start();
                 <li><hr class="dropdown-divider"></li>
                 <li><a class="dropdown-item" href="transfer.php">Transfer</a></li>
                  <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Reprocessing</a></li>
+                <li><a class="dropdown-item" href="drying.php">Drying</a></li>
                 <li><hr class="dropdown-divider"></li>
                 <li><a class="dropdown-item" href="hulling.php">Hulling</a></li> 
               </ul>

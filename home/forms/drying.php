8021a5d430177a72a468e6ad1c3a226bdd2a317e
isaac sys <isaacwasukira@gmail.com>
2022-11-09 21:21:36 +0300

    Updates

---
+++ home/forms/drying.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="stylesheet" href="styles.css">
+    
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DRYING FORM</title>
+    <style>
+      
+
+        </style>
+        </head>
+        <body>
+   <h1>DRYING FORM</h1>
+   <h2>CUSTOMER DETAILS: <input></h2>
+   CUSTOMER ID: <INput>
+   <div> Date:<input type="date">
+    <br>MC IN:<input>
+    <br>MC OUT:<input>
+    <br>Reference:<input>
+  </div container>
+    <br>
+   CUSTOMER NAME: <INput>
+   <br>CUSTOMER CONTACT: <INput>
+   <h2>DESCRIPTIONS</h2>
+  <table>
+    <tr><th>DETAILS</th>
+        <th>GRADE</th>
+        <th>QUANTITY</th>
+    </tr>
+    <tr>
+     <td>INPUT</td>
+<td><input></td>
+<td><input></td>
+    </tr>
+    <tr>
+        <td>OUTPUT</td>
+        <td><input></td>
+        <td><input></td>
+    </tr>
+    <tr>
+      <td>LOSS</td>
+      <td><input></td>
+      <td><input></td>
+    </tr>
+  </table>
+  <p>
+    <div class="div1">Prepared by;</div>
+    <div class="div2">Received by;</div>
+    <div class="div3">Verified by;</div>
+    <br>
+    <div class="div1"><input></div>
+    <div class="input2"><input></div>
+    <div class="div6"><input></div>
+</p>
+</body>
+
+</html>

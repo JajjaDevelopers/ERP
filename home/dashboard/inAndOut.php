32521be7c3bc5e10c4d9e92dbc7193d638c4c53b
IsaacWasukira <isaacwasukira@gmail.com>
2022-11-16 23:13:07 +0300

    finalised the generation of data from the db for dashboard

--- home/dashboard/inAndOut.php
+++ home/dashboard/inAndOut.php
@@ -5,41 +5,88 @@ $username = $_SESSION["userName"];
 <?php include("../private/database.php");?>
 
 <?php
-$currentDate = date_create();
-$firstDate = date_sub($currentDate,date_interval_create_from_date_string("31 days"));
 $daysList = array();
-for ($x=0; $x<=31; $x++){
-    $nextDate = date_add($firstDate, date_interval_create_from_date_string($x." days"));
-    array_push($daysList, date_format($nextDate, "Y-m-d"));
+$currentDate = new DateTime();
+$currentDateString = $currentDate->format('Y-m-d');
+array_push($daysList, $currentDateString);
 
+for ($x=1; $x<=30; $x++){
+    $prevDay = $currentDate->sub(new DateInterval('P1D'));
+    $prevDateString = $prevDay->format('Y-m-d');
+    array_push($daysList, $prevDateString);
+    $currentDate = $prevDay;
 }
 
 $dailyReceivedList = array();
 $dailyDispatchedList = array();
-function getDailyQty($qtyColumn, $table, $dateColumn, $listStore){
+
+$receivedQuery = $conn->prepare("SELECT sum(grn_qty) AS qty FROM grn WHERE grn_date=?");
+function getDailyQty(){ //$qtyColumn, $table, $dateColumn, $listStore
     include("../private/database.php");
-    global $daysList;
-    $receivedQuery = $conn->prepare("SELECT sum($qtyColumn) AS qty FROM $table WHERE $dateColumn=?");
+    global $daysList, $dailyReceivedList, $receivedQuery;
+    
     for ($i=0; $i<count($daysList); $i++){
-        $date = $daysList[$i];
+        $date = $daysList[$i]; //'2022-11-09'; //$daysList[$i];
         $receivedQuery->bind_param("s", $date);
         $receivedQuery->execute();
         $receivedQuery->bind_result($dailyQty);
         $receivedQuery->fetch();
-        $receivedQuery->close();
-        array_push($listStore, $dailyQty);
+        if ($dailyQty == ""){
+            $dailyQty = 0;
+        }
+        array_push($dailyReceivedList, $dailyQty);
+    }
+    $receivedQuery->close();
+}
+getDailyQty();
+
+
+$dispatchedQuery = $conn->prepare("SELECT sum(dispatch_qty) AS qty1 FROM dispatch WHERE dispatch_date=?");
+function getDailyOut(){
+    include("../private/database.php");
+    global $daysList, $dailyDispatchedList, $dispatchedQuery;
+    
+    for ($i=0; $i<count($daysList); $i++){
+        $date1 = $daysList[$i]; //'2022-11-09'; //$daysList[$i];
+        $dispatchedQuery->bind_param("s", $date1);
+        $dispatchedQuery->execute();
+        $dispatchedQuery->bind_result($dailyQty);
+        $dispatchedQuery->fetch();
+        if ($dailyQty == ""){
+            $dailyQty = 0;
+        }
+        array_push($dailyDispatchedList, $dailyQty);
     }
 }
-
-
-getDailyQty("grn_qty", "grn", "grn_date", $dailyReceivedList);
-getDailyQty("dispatch_qty", "dispatch", "dispatch_date", $dailyDispatchedList);
+getDailyOut();
 
 $dates = json_encode($daysList);
 $dailyReceived = json_encode($dailyReceivedList);
 $dailyDispatched = json_encode($dailyDispatchedList);
 
 ?>
+
+
+<!-- The html below tests response of this data generated here to the index.php page -->
+<select>
+    <?php
+    for ($p=0; $p<count($dailyReceivedList); $p++){
+        ?>
+        <option><?= $dailyReceivedList[$p] ?></option>
+        <?php
+    }
+    ?>
+</select>
+<select>
+    <?php
+    for ($p=0; $p<count($dailyDispatchedList); $p++){
+        ?>
+        <option><?= $dailyDispatchedList[$p] ?></option>
+        <?php
+    }
+    ?>
+</select>
+First Date: <?= date_format($currentDate, "Y-m-d")  ?>
 
 <select>
     <?php
--- home/forms/index.php
+++ home/forms/index.php
@@ -2,12 +2,13 @@
  session_start();
 
  error_reporting(1);
- include ("../dashboard/inAndOut.php");
+ 
  if(isset($_SESSION["userName"]))
  {
   if((time()- $_SESSION["lastLoginTimestamp"])<900)//logs out user automatically after 15 minutes of inactivity
   {
     include_once("header.php");
+    include ("../dashboard/inAndOut.php");
     include_once("footer.php");
   }else
   {

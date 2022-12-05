4b2de39927fbef48b1b6147ec6cdf7ab2fd889f8
IsaacWasukira <isaacwasukira@gmail.com>
2022-12-05 21:14:23 +0300

    updates

--- home/forms/newCustomer.php
+++ home/forms/newCustomer.php
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-<?php
-  session_start();
-
-  error_reporting(1);
-  if(isset($_SESSION["userName"]))
-  {
-    ?>
-    
-        <?php include_once ('header.php'); ?>
-        <body>
-            <form class="regularForm" method="POST" action="addNewCustomer.php">
-                <label for="customerId">Customer ID</label>
-                <input type="number" name="customerId" class="shortInput" readonly><br>
-                <label for="customerName">Customer Name</label>
-                <input type="text" name="customerName" class="longInputField"><br>
-                <label for="customerCity">City</label>
-                <input type="text" name="customerCity" class="longInputField"><br>
-                <label for="customerTel">Telephone</label>
-                <input type="number" name="customerTel" class="longInputField"><br>
-                <label for="customerEmail">Email</label>
-                <input type="email" name="customerEmail" class="longInputField"><br>
-                <input type="submit" class="controlButtons">
-            </form>
-        <?php include_once ('footer.php'); ?>
-        
-    <?php
-  }else
-  {
-    include "redirect.php";
-  }
-?>
-
-=======
 <?php include_once ('header.php'); ?>
 <body>
     <form class="regularForm" method="POST" action="addNewCustomer.php">
@@ -48,4 +14,3 @@
         <input type="submit" class="controlButtons">
     </form>
 <?php include_once ('footer.php'); ?>
->>>>>>> refs/remotes/origin/main

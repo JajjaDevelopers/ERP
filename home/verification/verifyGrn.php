690b8c569ebaef36fa6003b4205bc166b8f98ddd
IsaacWasukira <isaacwasukira@gmail.com>
2022-12-30 22:40:13 +0300

    updates

--- home/verification/verifyGrn.php
+++ home/verification/verifyGrn.php
@@ -11,7 +11,7 @@ $grnNo = formatDocNo($grn_no, "GRN-");
 <form class="regularForm" action="../connection/grnVerifyFinal.php" method="POST" style="height: fit-content;">
 
   <?php include "../forms/grnTemplate.php" ?>
-  <?php submitButton("Verify");?>
+  <?php submitButton("Verify", "Submit");?>
 </form>
 
 <?php include "../forms/footer.php" ?>
